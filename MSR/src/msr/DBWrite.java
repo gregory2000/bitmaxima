859109de5bbe8c16cb4ef6c87e09fc52832e95a5
George Gregory <g42gregory@Georges-MacBook-Pro-2.local>
2013-08-11 11:08:02 -0700

    ebean jar files, authors table

--- MSR/src/msr/DBWrite.java
+++ MSR/src/msr/DBWrite.java
@@ -10,6 +10,11 @@ import java.io.ObjectOutputStream;
 import com.microsoft.research.Author;
 import com.microsoft.research.PagedList;
 
+import tables.Authors;
+
+import com.avaje.ebean.Ebean;  
+import com.avaje.ebean.config.GlobalProperties;
+
 /**
  * 
  * @author g42gregory
@@ -17,6 +22,47 @@ import com.microsoft.research.PagedList;
  */
 public class DBWrite 
 {
+	
+	public static void testDB()
+	{
+		//System.setProperty("catalina.base", "D:/apps/tomcat6");  
+        System.setProperty("ebean.props.file", "ebean.properties");  
+        GlobalProperties.put("ebean.debug.sql", "true");  
+    
+    		//whether or not drop table at the beginning of the run: 
+    		GlobalProperties.put("ebean.ddl.run", "true");
+    	
+        Authors e = new Authors();  
+        e.setName("test1");  
+        e.setDescription("something1");  
+          
+        // will insert  
+        Ebean.save(e);  
+          
+        e.setDescription("changed1");  
+          
+        // this will update  
+        Ebean.save(e);  
+        
+        Authors q = new Authors();  
+        q.setName("test2");  
+        q.setDescription("something2");  
+          
+        // will insert  
+        Ebean.save(q); 
+        
+        
+        
+        // find the inserted entity by its id  
+        Authors e2 = Ebean.find(Authors.class, e.getId());  
+        System.out.println("Got "+e2.getDescription());  
+          
+        //Ebean.delete(e);  
+        // can use delete by id when you don't have the bean  
+        //Ebean.delete(ESimple.class, e.getId());  
+		
+	}
+	
 	private static void read(ObjectInputStream in) throws ClassNotFoundException, IOException 
 	{
 		for (int i = 0; i < 6000; i++)
@@ -44,9 +90,11 @@ public class DBWrite
 	
 	public static void main(String[] arg) throws FileNotFoundException, IOException, ClassNotFoundException
 	{
-		ObjectInputStream in = new ObjectInputStream(new FileInputStream("/Users/g42gregory/Dropbox/1_AI_People/data/msasearch/cs_authors/cs_authors_900001_1000000.dat"));
-		read(in);
-		in.close();
+		
+		
+		//ObjectInputStream in = new ObjectInputStream(new FileInputStream("/Users/g42gregory/Dropbox/1_AI_People/data/msasearch/cs_authors/cs_authors_900001_1000000.dat"));
+		//read(in);
+		//in.close();
 	}
 
 
