ec0a89dcd58d318f61c365cb4e9525208dff538d
jasonz <jasonz@gmail.com>
2013-09-02 21:06:31 -0700

    checkin for example code of using play framework
    example code of using play framework, a simple bookmarking application.
    
    (released under Apache License, Version 2.0)

---
+++ saveme.play/conf/evolutions/default/1.sql
@@ -0,0 +1,29 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table bookmark (
+  id                        bigint not null,
+  title                     varchar(255),
+  url                       varchar(255),
+  description               varchar(255),
+  image_url                 varchar(255),
+  constraint pk_bookmark primary key (id))
+;
+
+create sequence bookmark_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists bookmark;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists bookmark_seq;
+
